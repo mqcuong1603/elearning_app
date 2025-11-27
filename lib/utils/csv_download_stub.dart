919aba36c73cbacacd439fb28b6db31561638e0e
MonkeyNerdCoding <sockhi2004@gmail.com>
2025-11-27 13:43:53 +0700

    Clean branch

--- lib/utils/csv_download_stub.dart
+++
@@ -1,5 +0,0 @@
-/// Stub file for CSV download functionality
-/// This file is used when neither web nor IO implementations are available
-Future<String?> downloadCsv(String csvString, String filename) async {
-  throw UnsupportedError('CSV download is not supported on this platform');
-}
